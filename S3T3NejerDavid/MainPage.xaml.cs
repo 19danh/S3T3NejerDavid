15677403d6d13be436885104e52f5419978dedb1
19danh <danh.19@hotmail.com>
2021-10-27 13:23:28 -0500

    Agregar archivos de proyecto.

---
+++ S3T3NejerDavid/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace S3T3NejerDavid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(string usuario)
+        {
+            InitializeComponent();
+            lblUser.Text = "Bienvenido " + usuario;
+            DisplayAlert("Saludo", "Bienvenido " + usuario, "Ok");
+        }
+
+        private void btnSumar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double n1 = Convert.ToDouble(txtValorUno.Text);
+                double n2 = Convert.ToDouble(txtValorDos.Text);
+                double res = n1 + n2;
+                txtResultado.Text = Convert.ToString(res);
+
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "ok");
+            }
+        }
+    }
+}
---
+++ S3T3NejerDavid/login.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S3T3NejerDavid
+{
+   
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string clave = "estudiante2021";
+                string user = "uisrael2021";
+                if (clave == txtClave.Text && user == txtUsuario.Text)
+                {
+                    await Navigation.PushAsync(new MainPage(txtUsuario.Text));
+                }
+                else
+                {
+                    await DisplayAlert("Alerta", "Credenciales Incorrectas", "Ok");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Alerta", ex.Message, "Ok");
+                throw;
+            }
+
+        }
+    }
+}
